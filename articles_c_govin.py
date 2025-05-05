fc8fac40568e0f79a3e54bfb512b37a7e83069b2
Govindarajen <coodien21@gmail.com>
2025-05-05 16:44:56 +0200

    get articles in multiples pages

--- articles_c_govin.py
+++ articles_c_govin.py
@@ -6,7 +6,6 @@ db = client["ipssi-scrapping"]
 articles_collection = db["articles"]
 
 def fetch_articles(url):
-
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
     }
@@ -23,76 +22,81 @@ def fetch_articles(url):
             nav_links = navbar.find_all('a', href=True)
             for nav_link in nav_links:
                 section_url = nav_link['href']
-                print(nav_link)
                 if not section_url.startswith('http'):
                     section_url = requests.compat.urljoin(url, section_url)
-                try:
-                    section_response = requests.get(section_url, headers=headers)
-                    section_response.raise_for_status()
-                    section_soup = BeautifulSoup(section_response.text, 'html.parser')
 
-                    section_articles = section_soup.find_all('article')
-                    for article in section_articles:
-                        title = article.find('h3', class_='entry-title').get_text(strip=True) if article.find('h3', class_='entry-title') else None
-                        thumbnail = article.find('img', class_='attachment-thumbnail') if article.find('img', class_='attachment-thumbnail') else None
-                        thumbnail = thumbnail['data-lazy-src'] if thumbnail and 'data-lazy-src' in thumbnail.attrs else None
-                        category = nav_link.get_text(strip=True) if nav_link else None
-                        content = None
-                        sub_category = section_soup.find('span', class_='favtag').get_text(strip=True) if section_soup.find('span', class_='favtag') else None
-                        summary = None
-                        date = None
-                        author = None
-                        images = []
+                for page in range(1, 3):
+                    paginated_url = f"{section_url}/page/{page}"
+                    category = nav_link.get_text(strip=True) if nav_link else None
 
-                        link = article.find('header', class_='entry-header').find('a')['href'] if article.find('header', class_='entry-header') and article.find('header', class_='entry-header').find('a') and 'href' in article.find('header', class_='entry-header').find('a').attrs else None
-                        if link:
-                            try:
-                                article_response = requests.get(link, headers=headers)
-                                article_response.raise_for_status()
-                                article_soup = BeautifulSoup(article_response.text, 'html.parser')
+                    print(f"--- Scraping Articles in page {page} of category : {category} ---")
+                    try:
+                        section_response = requests.get(paginated_url, headers=headers)
+                        section_response.raise_for_status()
+                        section_soup = BeautifulSoup(section_response.text, 'html.parser')
 
-                                article_summary_div = article_soup.find('div', class_='article-hat')
-                                if article_summary_div:
-                                    summary = article_summary_div.find('p').get_text(strip=True) if article_summary_div.find('p') else None
+                        section_articles = section_soup.find_all('article')
+                        if not section_articles:
+                            break
 
-                                author = article_soup.find('span', class_='byline').get_text(strip=True)
+                        for article in section_articles:
+                            title = article.find('h3', class_='entry-title').get_text(strip=True) if article.find('h3', class_='entry-title') else None
+                            thumbnail = article.find('img', class_='attachment-thumbnail') if article.find('img', class_='attachment-thumbnail') else None
+                            thumbnail = thumbnail['data-lazy-src'] if thumbnail and 'data-lazy-src' in thumbnail.attrs else None
+                            content = None
+                            sub_category = section_soup.find('span', class_='favtag').get_text(strip=True) if section_soup.find('span', class_='favtag') else None
+                            summary = None
+                            date = None
+                            author = None
+                            images = []
 
-                                date_element = article_soup.find('time', class_='entry-date')
-                                if date_element and 'datetime' in date_element.attrs:
-                                    date = date_element['datetime']
-                                    date = date[:10]
+                            link = article.find('header', class_='entry-header').find('a')['href'] if article.find('header', class_='entry-header') and article.find('header', class_='entry-header').find('a') and 'href' in article.find('header', class_='entry-header').find('a').attrs else None
+                            if link:
+                                try:
+                                    article_response = requests.get(link, headers=headers)
+                                    article_response.raise_for_status()
+                                    article_soup = BeautifulSoup(article_response.text, 'html.parser')
 
+                                    article_summary_div = article_soup.find('div', class_='article-hat')
+                                    if article_summary_div:
+                                        summary = article_summary_div.find('p').get_text(strip=True) if article_summary_div.find('p') else None
 
-                                article_content = article_soup.find('div', class_='entry-content')
-                                if article_content:
-                                    content = ' '.join(p.get_text(strip=True) for p in article_content.find_all('p'))
-                                    
+                                    author = article_soup.find('span', class_='byline').get_text(strip=True)
 
-                                image_tags = article_soup.find_all('img')
-                                for img in image_tags:
-                                    img_url = img.get('data-lazy-src')
-                                    img_caption = img.get('alt') or img.get('title') or None
-                                    if img_url:
-                                        images.append({"url": img_url, "caption": img_caption})
+                                    date_element = article_soup.find('time', class_='entry-date')
+                                    if date_element and 'datetime' in date_element.attrs:
+                                        date = date_element['datetime']
+                                        date = date[:10]
 
-                            except requests.exceptions.RequestException as e:
-                                print(f"Error fetching the article URL: {e}")
+                                    article_content = article_soup.find('div', class_='entry-content')
+                                    if article_content:
+                                        content = ' '.join(p.get_text(strip=True) for p in article_content.find_all('p'))
 
-                        articles_data.append({
-                            "title": title,
-                            "thumbnail": thumbnail,
-                            "category": category,
-                            "sub_category": sub_category,
-                            "summary": summary,
-                            "author": author,
-                            "date": date,
-                            "images": images,
-                            "content": content,
-                        })
+                                    image_tags = article_soup.find_all('img')
+                                    for img in image_tags:
+                                        img_url = img.get('data-lazy-src')
+                                        img_caption = img.get('alt') or img.get('title') or None
+                                        if img_url:
+                                            images.append({"url": img_url, "caption": img_caption})
 
-                except requests.exceptions.RequestException as e:
-                    print(f"Error fetching the section URL: {e}")
+                                except requests.exceptions.RequestException as e:
+                                    print(f"Error fetching the article URL: {e}")
 
+                            articles_data.append({
+                                "title": title,
+                                "thumbnail": thumbnail,
+                                "category": category,
+                                "sub_category": sub_category,
+                                "summary": summary,
+                                "author": author,
+                                "date": date,
+                                "images": images,
+                                "content": content,
+                            })
+
+                    except requests.exceptions.RequestException as e:
+                        print(f"Error fetching the section URL: {e}")
+        print(f"Total Articles: {len(articles_data)}")
         return articles_data
 
     except requests.exceptions.RequestException as e:
@@ -104,11 +108,4 @@ url = "https://www.blogdumoderateur.com/"
 articles = fetch_articles(url)
 
 for i, article in enumerate(articles, 1):
-
-
-    """ articles_collection.insert_one(article) """
-    
-    print(f"\nArticle {i}:")
-    for key, value in article.items():
-        print(f"{key.capitalize()}: {value}")
- 
+    articles_collection.insert_one(article)
