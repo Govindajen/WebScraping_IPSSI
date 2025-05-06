dc590e7b36403ab52e9cbb8204462c3ccf440a22
Govindarajen <coodien21@gmail.com>
2025-05-06 12:05:27 +0200

    tp J2 - to fix

---
+++ TP2/doctlib_c_govin.py
@@ -0,0 +1,75 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+""" def getDoctors (number, location, type, assurance_type, consultation_type, price_range): """
+def getDoctors (type , location):
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service)
+    wait = WebDriverWait(driver, 20)
+
+    try: 
+        driver.get("https://www.doctolib.fr/")
+
+        try:
+            refuseBtn = wait.until(
+                EC.element_to_be_clickable((By.ID,
+                    "didomi-notice-disagree-button")))
+            
+            refuseBtn.click()
+
+            wait.until(EC.invisibility_of_element_located((By.ID,
+                    "didomi-notice-disagree-button")))
+        except:
+            pass
+            
+
+        wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR,
+                "input.searchbar-input.searchbar-query-input")))
+        
+        wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, 
+                "input.searchbar-input.searchbar-place-input")))
+        
+        location_input = wait.until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR,
+                "input.searchbar-input.searchbar-place-input")))
+        
+        location_input.clear()
+        location_input.send_keys(location)
+
+        wait.until(
+            EC.text_to_be_present_in_element_value((By.CSS_SELECTOR,
+                "input.searchbar-input.searchbar-place-input"),
+                location))
+
+        type_input = wait.until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR,
+                "input.searchbar-input.searchbar-query-input")))
+
+        type_input.clear()
+        type_input.send_keys(type)
+
+        wait.until(
+            EC.text_to_be_present_in_element_value((By.CSS_SELECTOR,
+                "input.searchbar-input.searchbar-query-input"),
+            type))
+        
+
+        location_input.send_keys(Keys.ENTER)
+        
+        time.sleep(10)
+    finally:
+        driver.quit()
+
+getDoctors(
+        type = "generaliste",
+        location = "75005"
+        )
